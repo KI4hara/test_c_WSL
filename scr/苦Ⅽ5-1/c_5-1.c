8ac645adec0ed332aabd2386498d5a2a5a4f5fba
KI4hara <shi1ta90ch@gmail.com>
2026-03-02 01:00:28 +0900

    5-1

---
+++ scr/苦Ⅽ5-1/c_5-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float left, right;
+    left = 10;
+    right = 3;
+    printf("%f\n", left + right);
+    printf("%f\n", left - right);
+    printf("%f\n", left * right);
+    printf("%f\n", left / right);
+    return 0;
+}
